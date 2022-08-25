4233f40bfc7c2fdb86e41b067d85554b4194a3bd
DaviMPaiva <davimpaiva@outlook.com>
2022-08-25 14:30:16 -0300

    added settings menu

--- course-plataform/src/app/app.module.ts
+++ course-plataform/src/app/app.module.ts
@@ -6,18 +6,30 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
+import { SettingsComponent } from './settings/settings.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HeaderComponent,
-    PopUpComponent
+    PopUpComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
