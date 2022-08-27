e4308b52c48334e8ac850d19452e8759a67fb7ee
OnLeonidas <michelleonidas81@gmail.com>
2022-08-27 16:13:41 -0300

    updating pages and creating components

--- course-plataform/src/app/app.module.ts
+++ course-plataform/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './pages/menu/menu.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CoursesComponent } from './pages/courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    HeaderComponent,
-    PopUpComponent
+    PopUpComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
