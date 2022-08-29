9c9849db56bb8064d0604daf22859be5e3dcea7b
OnLeonidas <michelleonidas81@gmail.com>
2022-08-29 17:26:40 -0300

    updated home

--- course-plataform/src/app/app-routing.module.ts
+++ course-plataform/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './sharepage/menu/menu.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CoursesComponent } from './pages/courses/courses.component';
 
 const routes: Routes = [
  {path:'',component:HomeComponent},
  {path:'Menu', component:MenuComponent},
- {path: 'login', component:LoginComponent}
+ {path: 'login', component:LoginComponent},
+ {path: 'Courses', component:CoursesComponent}
 ];
 
 @NgModule({
