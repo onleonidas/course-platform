7ae48696982769f2680e381c0f14200d5df7e817
DaviMPaiva <davimpaiva@outlook.com>
2022-09-01 20:28:41 -0300

    first working unity test

--- test/features/is_it_friday_yet.feature
+++ test/features/is_it_friday_yet.feature
@@ -1,5 +1,11 @@
-Feature: Is it Friday yet?
-  Everybody wants to know when it's Friday
+Feature: Client promotion notification 
 
-  Scenario: Sunday isn't Friday
-    Given today is Sunday
+	As a client I want to receive notifications when promotions are available  
+
+
+Scenario: The client can visualize his notification history 
+    Given I am at the configurations page
+    And I see a notifications history button
+    When I click the click the notifications history button
+    Then I go to the notifications history page 
+    And I see all my notification ever sent 
--- test/features/step_definitions/stepdefs.js
+++ test/features/step_definitions/stepdefs.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Given, When, Then} = require('@cucumber/cucumber');
 const { defineSupportCode } = require('cucumber');
 const { browser, $, element, ElementArrayFinder, by } = require('protractor');
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
@@ -7,18 +7,44 @@ const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const chromedriver = require('chromedriver');
 
-async function isItFriday(today) {
+async function starter() {
   //chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
   this.driver = new Builder()
         .forBrowser('chrome')
         .build();
+  this.driver.wait(until.elementLocated(By.tagName('h1')));
   await driver.get("http://localhost:4200/");
   //await expect(browser.getTitle()).to.eventually.equal('TaGui');
   //await $("a[name='alunos']").click();
 }
-Given('today is Sunday', async function () {
-  this.today = 'Sunday';
-  await isItFriday('Sunday');
+
+async function get_start_journey(){
+  await this.driver.findElement({ id: 'searchText' });
+}
+async function click_start_journey(){
+  this.driver.findElement({ id: 'searchText' }).click();
+}
+
+Given('I am at the configurations page',async function () {
+  starter();
+});
+Given('I see a notifications history button',async function () {
+  get_start_journey();
+});
+When('I click the click the notifications history button', function () {
+  click_start_journey();
+});
+Then('I go to the notifications history page', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+Then('I see all my notification ever sent', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+After(async function() {
+    await this.driver.close();
 });
 
