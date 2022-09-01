3b101a68c64157c2498916f54f574205a0d12f9f
dmp <dmp@cin.ufpe.br>
2022-09-01 08:53:59 -0300

    route changes

--- course-plataform/src/app/app.routes.ts
+++ course-plataform/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './pages/login/login.component'
 import { RegisterComponent } from './pages/register/register.component'
 import { BuycourseComponent } from './pages/buycourse/buycourse.component'
 import { PaymentComponent } from './pages/payment/payment.component'
+import { SettingsComponent } from './pages/settings/settings.component'
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'Login', component: LoginComponent },
     { path: 'Register', component: RegisterComponent },
     { path: 'Buycourse', component: BuycourseComponent },
-    { path: 'Payment', component: PaymentComponent}
+    { path: 'Payment', component: PaymentComponent},
+    { path: 'settings', component: SettingsComponent}
 ]
