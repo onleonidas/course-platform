3ae06c3a0736d6887ec897ae5bf4ee235f48dc10
Michel Leonidas <60946868+onleonidas@users.noreply.github.com>
2022-09-01 21:35:55 -0300

    deleting unusable folder

--- test/todo-spec.js
+++
@@ -1,9 +0,0 @@
-describe('test my site', function() {
-    it('should add a todo', function() {
-      browser.get('http://localhost:4200/');
-  
-      element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-      element(by.css('[value="add"]')).click();
-
-    });
-  });
