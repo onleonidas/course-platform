3a9a1693abb68c2592553a08f889e1c26f8fb74b
dmp <dmp@cin.ufpe.br>
2022-09-13 14:53:14 -0300

    features code base

---
+++ server/notificacao/notificacao.js
@@ -0,0 +1,1 @@
+exports.perimeter = function(width) { return 4 * width; };
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const http = require("http");
 const express = require("express");
-
+const notificacao = require("./notificacao/notificacao.js")
+const autenticacao = require("./autenticacao/autenticacao.js")
+const autenticacao = require("./cadastrar_cursos/cadastro.js")
+const autenticacao = require("./certificado/certificado.js")
+const autenticacao = require("./compra/compra.js")
 
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("<h1>Servidor rodando com ExpressJS</h1>");
+app.get("/hmm", function(req, res) {
+    res.send('The area of a square with a width of 10 is ' + mod.perimeter(10));
 });
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
+
