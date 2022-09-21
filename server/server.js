7fffe627e8fb37ed9f716cb5650fd66c26d8139a
DaviMPaiva <davimpaiva@outlook.com>
2022-09-21 16:40:10 -0300

    now server and client are working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const fs = require('fs')
+const cors = require('cors')
 
 const notificacao = require("./notificacao/notificacao.js");
 const autenticacao = require("./autenticacao/autenticacao.js");
@@ -11,12 +12,14 @@ const compra = require("./compra/compra.js");
 
 http.createServer(app).listen(3000,'localhost', () => console.log("Servidor rodando local na porta 3000"));
 
+app.use(cors())
+
 app.get("/hmm", function(req, res) {
     console.log(req.url,req.method);
 
         
     res.setHeader('Content-Type', 'text/html');
-    res.send('hmmmmm')
+    res.send(JSON.stringify('hmmmmm'))
 
     /*
     fs.readFile('../course-plataform/src/index.html', (err,data) => {
