ffa8f812a5252e84e81824a328ee33cff27a2f18
Michel Leonidas <mlas3@cin.ufpe.br>
2022-09-25 19:45:13 -0300

    update

--- course-plataform/src/app/pages/buycourse/buycourse.component.ts
+++ course-plataform/src/app/pages/buycourse/buycourse.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { json } from 'body-parser';
 import axios from 'axios';
-
 
 import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
@@ -15,24 +15,19 @@ import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser
 })
 export class BuycourseComponent implements OnInit {
   server = 'http://localhost:3000';
-  array_cursos: any[]
-  nomes: string[];
-  links: string[];
-  desc: string[];
-  imagens: string[];
-  constructor(private param: ActivatedRoute) {this.nomes = [];this.links = [];this.desc = [];this.imagens = [];
-    this.array_cursos = [];}
-  ngOnInit(): void {this.get_courses();}
-    
-  public open(name: String) {
-    alert('Open ' + name);
+
+  constructor(private param:ActivatedRoute) { }
+  getNameCourse:any;
+  courseData:any;
+
+  ngOnInit(): void {
+    this.getNameCourse = this.param.snapshot.paramMap.get('name');
   }
 
   async CourseInfo(){
-    console.log(this.param.snapshot.paramMap.get('name'))
+
     await axios.post((this.server + '/Buycourses'),{
-      coursename: (this.param.snapshot.paramMap.get('name')),
-      
+      coursename: (this.param.snapshot.paramMap.get('name'))
     })
     .then(function (response) {
       console.log(response);
@@ -42,25 +37,4 @@ export class BuycourseComponent implements OnInit {
     });
 
   }
-  
-  public pickid(id: String){
-    console.log(id)
-    return id;
-  }
-
-
-  async get_courses(){
-    const data1 = () => {return axios.put(this.server + '/Buycourse/{{}}', {})
-    .then(function (response) {
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    }
-    data1().then(data => {
-      this.array_cursos = data;
-    })
-  }
-  
 }
