b3cd39a5651462471c1e6c751fbeead1912fd717
DaviMPaiva <davimpaiva@outlook.com>
2022-09-24 22:13:14 -0300

    tentative of video player

--- course-plataform/src/app/pages/buycourse/buycourse.component.ts
+++ course-plataform/src/app/pages/buycourse/buycourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-buycourse',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buycourse.component.scss']
 })
 export class BuycourseComponent implements OnInit {
+  safeURL: any;
+  videoURL: SafeResourceUrl;
+  constructor(private _sanitizer: DomSanitizer){this.safeURL= "";this.videoURL = ""}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    let id = 'https://www.youtube.com/watch?v=m9QU69958JU&list=RDMMm9QU69958JU&start_radio=1&ab_channel=YudongWang';
+    let url = `https://www.youtube.com/embed/${id}`;
+    this.videoURL = this._sanitizer.bypassSecurityTrustResourceUrl(id);
+}
 
 }
