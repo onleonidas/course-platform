b8cb523b440c28a0f7cde11497f521abc1d93007
Michel Leonidas <mlas3@cin.ufpe.br>
2022-10-01 21:23:09 -0300

    uptade new register

--- course-plataform/src/app/pages/register/register.component.ts
+++ course-plataform/src/app/pages/register/register.component.ts
@@ -63,7 +63,7 @@ export class RegisterComponent implements OnInit {
       name: this.email,
       email: this.email,
       notfication_config:"I want to receive promotion notification",
-      is_professor: false,
+      is_professor: this.is_professor,
       courses_owned:[{"course_id":"","user_course_progress":0},]//lembrar de substituir.
     })
   }
